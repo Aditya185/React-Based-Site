b0c91b1b77d5b90e71dab6875c495f1558824a44
Aditya Prakash <adityaprakash185@gmail.com>
2020-02-20 21:55:35 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -8,6 +8,7 @@ import { Card, CardImg, CardImgOverlay,
 import { LocalForm, Control, Errors } from 'react-redux-form'
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 
@@ -45,7 +46,7 @@ function RenderComments({comments, addComment, dishId}) {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle>{dish.name}</CardTitle>
                             <CardText>{dish.description}</CardText>
